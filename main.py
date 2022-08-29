0923ac262bb39f63d33dc97e75036bdbb708eb65
EternalMemory672 <274588969@qq.com>
2022-08-29 16:37:15 +0800

    main

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from models import hostScanner
+from models import portScanner
+
 
 def main():
-    targets = input("Enter the ip range: ")
+    # targets = input("Enter the ip range: ")
     targets = "192.168.1.2-192.168.2.2"
     info = hostScanner.main(targets)
     print(info)
-    iplist = []
+    d = {}
+    for ip, sys in info.items():
+        ports = portScanner.main(ip)
+        d[ip] = [sys]+ports
+    print(d)
 
 
 if __name__ == '__main__':
