9c34abd90a89bfd21decae5cf52ef1ec9ca39315
lhl7 <75879657+lhl7@users.noreply.github.com>
2022-08-30 16:29:26 +0800

    修改项目结构和readme文件

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from models import hostScanner
 from models import portScanner
+from models import topoDrawer
+from models import vulFinder
+from models import appScanner
+import threading
 
+def output(text):
+    print(text)
 
 def main():
     # targets = input("Enter the ip range: ")
     targets = "192.168.1.2-192.168.2.2"
     info = hostScanner.main(targets)
-    print(info)
-    d = {}
-    for ip, sys in info.items():
+    output(info)
+    ipPortDict = {}
+    for ip, sysInfo in info.items():
         ports = portScanner.main(ip)
-        d[ip] = [sys]+ports
-    print(d)
+        ipPortDict[ip] = [sysInfo]+ports
+    print(ipPortDict)
 
 
 if __name__ == '__main__':
