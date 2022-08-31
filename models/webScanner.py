fbda6073d63e00a02c341a429eae24054f4ed37d
lhl7 <lhl_2507@qq.com>
2022-08-31 23:31:54 +0800

    完成后端所有接口

--- models/webScanner.py
+++ models/webScanner.py
@@ -1,20 +1,6 @@
-import sys
-sys.path.append('webScan')
-sys.path.append('models/webScan')
-from models.webScan import Scanner
+from models.webScan import WebScanner
 
+WebScanner.main("https://www.douban.com/",[])
 
-
-def main(ip, ports):
-    web_list = []
-    for port in ports:
-        target = ip + port
-        dic = dict()
-        dic['ip'] = ip
-        dic['port'] = port
-        res = Scanner.Sitadel(target).main()
-        for item in res:
-            if item[1] is not None:
-                dic[item[0]] = item[1]
-        web_list.append(dic)
-    return web_list
+def main():
+    return 0
