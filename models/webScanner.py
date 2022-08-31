29aa304a2e87693646466c4bc47f2102855a8317
Liang-Chaoyue <496658048@qq.com>
2022-08-31 23:33:37 +0800

    webScanner更新

--- models/webScanner.py
+++ models/webScanner.py
@@ -1,6 +1,20 @@
-from models.webScan import WebScanner
+import sys
+sys.path.append('webScan')
+sys.path.append('models/webScan')
+from models.webScan import Scanner
 
-WebScanner.main("https://www.douban.com/",[])
 
-def main():
-    return 0
+
+def main(ip, ports):
+    web_list = []
+    for port in ports:
+        target = ip + port
+        dic = dict()
+        dic['ip'] = ip
+        dic['port'] = port
+        res = Scanner.Sitadel(target).main()
+        for item in res:
+            if item[1] is not None:
+                dic[item[0]] = item[1]
+        web_list.append(dic)
+    return web_list
