efaf5129979c9c78252a7bc17dc32c9de76bc063
crazysword13 <1667654642@qq.com>
2022-09-02 03:13:01 +0800

    前端最新代码以及dist包

---
+++ front_source/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-main>
+      <!-- <router-view v-if="isReloadAlive" /> -->
+      <router-view v-if="isReloadAlive"></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  provide(){
+    return{
+      reload: this.reload
+    }
+  },
+  data(){
+     return{
+      isReloadAlive: true
+    }
+  },
+  methods: {
+    reload() {
+      this.isReloadAlive = false;
+      this.$nextTick(function(){
+        this.isReloadAlive = true;
+      })
+    }
+  }
+  // data: () => ({
+  //   //
+  // }),
+};
+</script>
