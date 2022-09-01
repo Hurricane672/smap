63aba9009bd7f16eb8234c3c3fa2966ba0b840ed
Liang-Chaoyue <496658048@qq.com>
2022-09-01 17:59:01 +0800

    webScanner更新

--- models/webScan/lib/modules/fingerprints/frontend/jquery.py
+++ models/webScan/lib/modules/fingerprints/frontend/jquery.py
@@ -4,17 +4,23 @@ from lib.modules.fingerprints import FingerprintPlugin
 
 class Jquery(FingerprintPlugin):
     def process(self, headers, content):
+        print(content)
+        print(headers)
         res = re.findall(r'jquery-([0-9\-.]+).js|jquery', content, re.I)
         res2 = re.findall(r'jquery/([0-9\-.]+)/', content, re.I)
         res3 = re.findall(r'jquery-([0-9\-.]+).min.js', content, re.I)
+        res4 = re.findall(r'jquery@([0-9.]+)/', content, re.I)
         if res is not None:
             for item in res:
                 if item != "":
-                    return "jquery" + item
+                    return "jquery " + item
             for item in res2:
                 if item != "":
-                    return "jquery" + item
+                    return "jquery " + item
             for item in res3:
                 if item != "":
-                    return "jquery" + item
+                    return "jquery " + item
+            for item in res4:
+                if item != "":
+                    return "jquery " + item
             return "jquery"
